c5b85dd605cf1690e9a26c32307dd3665baf8cba
Mohan Kaliyappan <mohan.k@contus.in>
2022-11-13 17:03:35 +0530

    Add group UI and validation completed

--- app/src/main/java/com/seabird/whatsdev/ui/addgroup/AddGroupFragment.kt
+++ app/src/main/java/com/seabird/whatsdev/ui/addgroup/AddGroupFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.seabird.whatsdev.R
 import com.seabird.whatsdev.databinding.FragmentAddGroupBinding
+import com.seabird.whatsdev.setSafeOnClickListener
 import com.seabird.whatsdev.ui.MainActivity
 
 class AddGroupFragment : Fragment() {
@@ -17,6 +20,10 @@ class AddGroupFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private var selectedPosition = 0
+
+    private val categories: List<String> by lazy { resources.getStringArray(R.array.categories).toList() }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,5 +32,43 @@ class AddGroupFragment : Fragment() {
         (activity as MainActivity).hideAddGroupAction()
         _binding = FragmentAddGroupBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpSpinnerView()
+        setUpListeners()
+    }
+
+    private fun setUpListeners() {
+        binding.addGroup.setSafeOnClickListener {
+            validateAndCreateGroup()
+        }
+    }
+
+    private fun setUpSpinnerView() {
+        binding.categorySpinner.setItems(categories)
+        binding.categorySpinner.setOnItemSelectedListener { _, position, _, _ ->
+            selectedPosition = position
+        }
+    }
+
+    private fun validateAndCreateGroup() {
+        if (selectedPosition == 0) {
+            Toast.makeText(requireContext(), getString(R.string.validation_category), Toast.LENGTH_SHORT).show()
+        } else if (binding.groupNameEditText.text.toString().isEmpty()){
+            Toast.makeText(requireContext(), getString(R.string.validation_group_name), Toast.LENGTH_SHORT).show()
+        } else if (binding.groupLinkEditText.text.toString().isEmpty()) {
+            Toast.makeText(requireContext(), getString(R.string.validation_group_link), Toast.LENGTH_SHORT).show()
+        } else if (binding.groupDescriptionEditText.text.toString().isEmpty()) {
+            Toast.makeText(requireContext(), getString(R.string.validation_group_description), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(requireContext(), getString(R.string.group_added), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
